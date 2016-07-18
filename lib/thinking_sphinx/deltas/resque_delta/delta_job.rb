f2f97739eebe7b199d1b00c79e2ddbad4a8474b7
Julio Monteiro <jmonteiro@jobscore.com>
2016-07-18 13:41:38 -0700

    Cleaning up similar jobs from the queue

--- lib/thinking_sphinx/deltas/resque_delta/delta_job.rb
+++ lib/thinking_sphinx/deltas/resque_delta/delta_job.rb
@@ -9,6 +9,11 @@ class ThinkingSphinx::Deltas::ResqueDelta::DeltaJob
   # @param [String] index the name of the Sphinx index
   #
   def self.perform(index)
+    cancel_equal_index_jobs(index)
     ThinkingSphinx::Deltas::IndexJob.new(index).perform
+  end
+
+  def self.cancel_equal_index_jobs(index)
+    Resque.redis.lrem "queue:#{@queue}", 0, "{\"class\":\"ThinkingSphinx::Deltas::ResqueDelta::DeltaJob\",\"args\":[\"#{index}\"]}"
   end
 end
